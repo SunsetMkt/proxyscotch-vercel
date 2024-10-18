d518017d707868e9019f82bbf4b3efc966e1c0e5
Sunset <26019675+SunsetMkt@users.noreply.github.com>
2024-10-18 18:15:37 +0800

    More ENVs

--- api/index.go
+++ api/index.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"net/http"
+	"os"
+	"strings"
 	_ "unsafe"
 
 	_ "github.com/hoppscotch/proxyscotch/libproxy"
@@ -10,10 +12,32 @@ import (
 //go:linkname proxyHandler github.com/hoppscotch/proxyscotch/libproxy.proxyHandler
 func proxyHandler(w http.ResponseWriter, r *http.Request)
 
+//go:linkname accessToken github.com/hoppscotch/proxyscotch/libproxy.accessToken
+var accessToken string
+
 //go:linkname allowedOrigins github.com/hoppscotch/proxyscotch/libproxy.allowedOrigins
 var allowedOrigins []string
 
+//go:linkname bannedOutputs github.com/hoppscotch/proxyscotch/libproxy.bannedOutputs
+var bannedOutputs []string
+
+//go:linkname bannedDests github.com/hoppscotch/proxyscotch/libproxy.bannedDests
+var bannedDests []string
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-	allowedOrigins = []string{"*"}
+	if os.Getenv("ACCESS_TOKEN") != "" {
+		accessToken = os.Getenv("ACCESS_TOKEN")
+	}
+	if os.Getenv("ALLOWED_ORIGINS") != "" {
+		allowedOrigins = strings.Split(os.Getenv("ALLOWED_ORIGINS"), ",")
+	} else {
+		allowedOrigins = []string{"*"}
+	}
+	if os.Getenv("BANNED_OUTPUTS") != "" {
+		bannedOutputs = strings.Split(os.Getenv("BANNED_OUTPUTS"), ",")
+	}
+	if os.Getenv("BANNED_DESTINATIONS") != "" {
+		bannedDests = strings.Split(os.Getenv("BANNED_DESTINATIONS"), ",")
+	}
 	proxyHandler(w, r)
 }
