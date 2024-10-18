6d3d670c794d7733df843a8b8be83a53809ef4be
Sunset <26019675+SunsetMkt@users.noreply.github.com>
2024-10-18 17:57:03 +0800

    fix

--- api/index.go
+++ api/index.go
@@ -5,7 +5,7 @@ import (
 	_ "unsafe"
 )
 
-//go:linkname proxyHandler github.com/hoppscotch/proxyscotch.libproxy.proxyHandler
+//go:linkname proxyHandler github.com/hoppscotch/proxyscotch/libproxy.proxyHandler
 func proxyHandler(w http.ResponseWriter, r *http.Request)
 
 func Handler(w http.ResponseWriter, r *http.Request) {
