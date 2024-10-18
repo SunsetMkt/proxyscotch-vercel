9770423c0add8621136a90d3baf3b1aacd6bb65d
Sunset <26019675+SunsetMkt@users.noreply.github.com>
2024-10-18 18:07:15 +0800

    allowedOrigins

--- api/index.go
+++ api/index.go
@@ -10,6 +10,10 @@ import (
 //go:linkname proxyHandler github.com/hoppscotch/proxyscotch/libproxy.proxyHandler
 func proxyHandler(w http.ResponseWriter, r *http.Request)
 
+//go:linkname allowedOrigins github.com/hoppscotch/proxyscotch/libproxy.allowedOrigins
+var allowedOrigins []string
+
 func Handler(w http.ResponseWriter, r *http.Request) {
+	allowedOrigins = []string{"*"}
 	proxyHandler(w, r)
 }
